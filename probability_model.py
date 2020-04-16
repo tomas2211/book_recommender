24c8cc6f5ac1bb924f211441993c198266db37f7
tomas2211 <tomas2211@post.cz>
2020-04-17 01:29:53 +0200

    Added error handling and more parameters

--- probability_model.py
+++ probability_model.py
@@ -26,34 +26,53 @@ plt.ylabel('Number of ratings')
 plt.show()
 
 
-RATING_NO_THRESH = 9
+TOTAL_RATINGS_THRESH = 9
 LIKE_THRESH = 5
+SIGMA_MUL = 1
+IMPLICIT_LIKES_AS_LIKE = False
 
-isbns_fil = cnts[cnts > RATING_NO_THRESH].index
-print('Number of books with >%d ratings: %d' %(RATING_NO_THRESH, len(isbns_fil)))
-rat = rat_explicit[rat_explicit.ISBN.isin(isbns_fil)]
 
-query_isbn = '0740718398' #isbns_fil[128714]
+isbns_fil = cnts[cnts > TOTAL_RATINGS_THRESH].index
+print('Number of books with >%d ratings: %d' %(TOTAL_RATINGS_THRESH, len(isbns_fil)))
+rat_exp = rat_explicit[rat_explicit.ISBN.isin(isbns_fil)]
+rat_all = ratings[ratings.ISBN.isin(isbns_fil)]
+
+query_isbn = isbns_fil[531]
 query_name = books[books.ISBN == query_isbn]['Book-Title'].item()
 print('Query book: %s' % query_name)
+print('Query ratings: %d' % rat_exp[rat_exp.ISBN == query_isbn]['Book-Rating'].count())
+print('Mean query rating: %.2f' % rat_exp[rat_exp.ISBN == query_isbn]['Book-Rating'].mean())
 
+if IMPLICIT_LIKES_AS_LIKE:
+    users_liking = rat_all[(rat_all.ISBN == query_isbn) &
+                           ((rat_all['Book-Rating'] > LIKE_THRESH) | (rat_all['Book-Rating'] == 0))]['User-ID']
+else:
+    users_liking = rat_exp[(rat_exp.ISBN == query_isbn) & (rat_exp['Book-Rating'] > LIKE_THRESH)]['User-ID']
+print('Users liking the query: %d' % len(users_liking))
 
-users_liking = rat[(rat.ISBN == query_isbn) & (rat['Book-Rating'] > LIKE_THRESH)]['User-ID']
-print('Users liking the book: %d' % len(users_liking))
-
-relevant_rat = rat[rat['User-ID'].isin(users_liking)]
+relevant_rat = rat_exp[rat_exp['User-ID'].isin(users_liking)]
 
 print('Relevant %d ratings.' % relevant_rat.shape[0])
 print('Deciding among %d books.' % len(relevant_rat.ISBN.unique()))
 
-cnt_ratings = relevant_rat.groupby('ISBN')['Book-Rating'].count()
-mean_ratings = relevant_rat.groupby('ISBN')['Book-Rating'].mean()#[cnt_ratings > 2]
-std_ratings = relevant_rat.groupby('ISBN')['Book-Rating'].std()#[cnt_ratings > 2]
+relevant_rat_grouped = relevant_rat.groupby('ISBN')['Book-Rating']
+cnt_ratings = relevant_rat_grouped.count()
+mean_ratings = relevant_rat_grouped.mean()
+if (cnt_ratings >= 2).sum() > 5:
+    mean_ratings = mean_ratings[cnt_ratings > 2]
+    std_ratings = relevant_rat_grouped.std()[cnt_ratings > 2]
+    book_scores = mean_ratings - SIGMA_MUL*std_ratings
+else:
+    book_scores = mean_ratings
 
-minimal_expected = (mean_ratings - 1*std_ratings).sort_values(ascending=False)
+book_scores = book_scores.sort_values(ascending=False)
 
-for i,r in pd.merge(minimal_expected, books, how='left', on='ISBN').head(5).iterrows():
+
+print('--- RECOMMENDING ---')
+for i,r in pd.merge(book_scores, books, how='left', on='ISBN').head(5).iterrows():
     print('%.1f - %s' % (r['Book-Rating'], r['Book-Title']))
 
-for i,r in pd.merge(minimal_expected, books, how='left', on='ISBN').tail(5).iterrows():
+
+print('--- NOT RECOMMENDED ---')
+for i,r in pd.merge(book_scores, books, how='left', on='ISBN').tail(5).iterrows():
     print('%.1f - %s' % (r['Book-Rating'], r['Book-Title']))
