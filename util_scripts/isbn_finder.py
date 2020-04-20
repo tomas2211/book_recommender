48a62c18a34f506a0987426a1785f14cf9ddf1ce
tomas2211 <tomas2211@post.cz>
2020-04-20 21:41:16 +0200

    Making ISBN finder a module

--- util_scripts/isbn_finder.py
+++ util_scripts/isbn_finder.py
@@ -1,44 +1,59 @@
 import pandas as pd
 from fuzzywuzzy import fuzz
 
-from data_utils import load_ratings
+
+class ISBN_Finder():
+    def __init__(self, books_csv_fn='data/BX-Books.csv', isbns_filter=None):
+        self.df_books = pd.read_csv(
+            books_csv_fn,
+            usecols=['ISBN', 'Book-Title', 'Book-Author'],
+            sep=';',
+            encoding='latin-1',
+            index_col='ISBN'
+        )
+
+        if isbns_filter is not None:
+            self.df_books = self.df_books[self.df_books.index.isin(isbns_filter)]
+
+    def _match_name(self, name: str):
+        name = name.lower()
+        return self.df_books['Book-Title'].apply(lambda x: fuzz.partial_ratio(x.lower(), name))
+
+    def name2isbn_top5(self, name: str):
+        return self._match_name(name).sort_values().index[-1:-6:-1].array
+
+    def name2isbn(self, name: str):
+        return self._match_name(name).idxmax()
+
+    def isbn2name(self, isbn: str, softerror=True):
+        try:
+            return '{}: {}'.format(self.df_books.loc[isbn]['Book-Author'], self.df_books.loc[isbn]['Book-Title'])
+        except KeyError:
+            if softerror:
+                return '<N/A>: <N/A>'
+            else:
+                return None
 
 
-def find_book_isbn(name: str, books: pd.DataFrame):
-    name = name.lower()
-    distances = books['Book-Title'].apply(lambda x: fuzz.partial_ratio(x.lower(), name))
-    return distances.sort_values().index[-1:-5:-1].array
+if __name__ == '__main__':
+    from data_utils import load_ratings
 
+    ratings, isbns_filtered, test_useridx, train_useridx = load_ratings()
 
-df_books = pd.read_csv(
-    '../data/BX-Books.csv',
-    usecols=['ISBN', 'Book-Title', 'Book-Author'],
-    sep=';',
-    encoding='latin-1',
-    index_col='ISBN'
-)
+    isbn_finder = ISBN_Finder(isbns_filter=isbns_filtered)
 
-ratings, isbns_filtered, test_useridx, train_useridx = load_ratings()
-
-# Filter ratings to explicit and by isbns_filtered
-rat_explicit = ratings[ratings['Book-Rating'] != 0]
-rat_exp = rat_explicit[rat_explicit.ISBN.isin(isbns_filtered)]
-rat = ratings[ratings.ISBN.isin(isbns_filtered)]
-
-df_books_fil = df_books[df_books.index.isin(isbns_filtered)]
-
-name = input('Enter book name:')
-while name.lower() != 'end':
-    isbns = find_book_isbn(name, df_books_fil)
-
-    for isbn in isbns:
-        isbn_rat = rat_exp[rat_exp.ISBN == isbn]
-        rat_num = len(isbn_rat)
-        meanrat = isbn_rat['Book-Rating'].mean()
-
-        q_name = df_books.loc[isbn]['Book-Title']
-        q_author = df_books.loc[isbn]['Book-Author']
-
-        print('Found: %s: %s - %s; mean rating %.2f (%d)' % (q_author, q_name, isbn, meanrat, rat_num))
+    # Filter ratings to explicit and by isbns_filtered
+    rat_exp = ratings[ratings['Book-Rating'] != 0]
+    rat_exp = rat_exp[rat_exp.ISBN.isin(isbns_filtered)]
 
     name = input('Enter book name:')
+    while name.lower() != 'end':
+        isbns = isbn_finder.name2isbn_top5(name)
+        for isbn in isbns:
+            isbn_rat = rat_exp[rat_exp.ISBN == isbn]
+            rat_num = len(isbn_rat)
+            meanrat = isbn_rat['Book-Rating'].mean()
+            found_name = isbn_finder.isbn2name(isbn)
+            print('Found: %s - %s; mean rating %.2f (%d)' % (found_name, isbn, meanrat, rat_num))
+
+        name = input('Enter book name:')
